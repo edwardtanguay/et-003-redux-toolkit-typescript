79ff17618c238952a0149693af67801746c25a29
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-15 11:41:26 +0100

    showing books

--- src/common/pages/PageBooks.tsx
+++ src/common/pages/PageBooks.tsx
@@ -1,18 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { add, subtract } from '../../features/cart/cartSlice';
+import { addCartItem } from '../../features/cart/cartSlice';
 import { RootState } from '../../app/store';
 
 export const PageBooks = () => {
 	const dispatch = useDispatch();
 	const selectCart = (state: RootState) => state.cart;
-	const { count } = useSelector(selectCart);
+	const { books, cartItems } = useSelector(selectCart);
 
 	return (
 		<div className="pageBooks">
-			<button onClick={() => dispatch(subtract())}>Subtract One</button>
-			<button onClick={() => dispatch(add())}>Add One</button>
-			<hr />
-			<div>count: {count}</div>
+			<div className="books">
+				{books.map((book) => {
+					return (
+						<div className="book" key={book.id}>
+							<img src={`images/${book.idCode}.jpg`} />
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
