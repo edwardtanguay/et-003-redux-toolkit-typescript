a5eb074074324405a875d086853a052d2703267f
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-15 10:12:11 +0100

    created common directory

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import './App.scss';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
-import { PageWelcome } from '../components/PageWelcome';
-import { PageBooks } from '../components/PageBooks';
-import { PageCart } from '../components/PageCart';
+import { PageWelcome } from '../common/pages/PageWelcome';
+import { PageBooks } from '../common/pages/PageBooks';
+import { PageCart } from '../common/pages/PageCart';
 
 function App() {
 	return (
